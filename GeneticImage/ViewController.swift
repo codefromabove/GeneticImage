9cbd79038b5cac07c5408c09b6f4a6c76143a727
Denis Lebedev <d2.lebedev@gmail.com>
2014-12-09 18:21:15 +0300

    Initial Commit

---
+++ GeneticImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeneticImage
+//
+//  Created by Dzianis Lebedzeu on 12/9/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
